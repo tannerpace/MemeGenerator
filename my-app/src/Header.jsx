72abd46a2a2a882e169acb6a413c55c718fe2b4e
TannerB <tannerpace@gmail.com>
2021-09-25 19:55:00 -0400

    adds MUI

---
+++ my-app/src/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+function Header() {
+    return (
+        <header>
+            <img 
+                src="https://t4.ftcdn.net/jpg/02/76/08/07/360_F_276080724_hltnCyDjcqAyRtLzDYo3T2jXbBtCD7fl.jpg" 
+                alt="Laf"
+            />
+            <p>Meme!😃 Maker</p>
+        </header>
+    )
+}
+
+export default Header
--- my-app/src/app.js
+++ my-app/src/app.js
@@ -1,13 +1,16 @@
 import React from "react"
 import Header from "./Header"
 import MemeGenerator from "./MemeGenerator"
+import Box from "@mui"
 
 function App() {
     return (
+        <Box>
         <div>
             <Header />
             <MemeGenerator />
         </div>
+        </Box>
     )
 }
 
